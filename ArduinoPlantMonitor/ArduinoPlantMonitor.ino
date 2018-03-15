1b4c617b16061b338e42bbdef518c0bfd8f09036
daniel.wikander <31259018+danielwikander@users.noreply.github.com>
2018-03-16 00:52:24 +0100

    Added v1 of the arduino sketch.
    First version of the arduino sketch.

---
+++ ArduinoPlantMonitor/ArduinoPlantMonitor.ino
@@ -0,0 +1,41 @@
+
+// Imports
+#include <dht11.h>    // Library for DHT11 temperature and airmoisture sensor.
+
+// Variables
+int soilMoistureSensor = A0;  // Soil-moisture sensor on analog pin A0.
+int ledPin = 13;              // Built in arduino LED pin.
+int soilMoistureValue = 0;    // variable to store the value coming from the sensor
+
+dht11 DHT11;          // Declares the DHT sensor as an object.
+#define DHT11PIN 7    // Declares the pin number for the DHT sensor.
+
+// Main
+void setup() {
+  pinMode(ledPin, OUTPUT); // Declares the arduino LED as an output.
+  Serial.begin(9600);      // Sets serial communication datarate to 9600 baud.
+}
+
+void loop() {
+
+  // Soil moisture sensor stuff.
+  soilMoistureValue = analogRead(soilMoistureSensor); // reads the value from the soil sensor
+  int percentageSoilMoistureValue = map(soilMoistureValue, 1023, 200, 0, 100); // Converts the value to %
+  
+  // Air temperature and humidity sensor stuff.
+  int chk = DHT11.read(DHT11PIN); // Reads the value from the sensor.
+
+  // Prints all values
+  Serial.print("Soil moisture level (%): ");
+  Serial.println(percentageSoilMoistureValue); // Prints the soil moisture level.
+  
+  Serial.print("Air humidity level (%):  ");
+  Serial.println((float)DHT11.humidity);    // Prints air humidity level.
+
+  Serial.print("Air temperature (C):     ");
+  Serial.println((float)DHT11.temperature); // Prints air temperature.
+
+  // Pause before next reading.
+  Serial.println("------------------------------");
+  delay(5000);
+}
