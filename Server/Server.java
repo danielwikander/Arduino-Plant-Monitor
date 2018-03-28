b9d4da72c85022349f17a96ea0009972f9d2a0b4
Daniel Wikander <daniel.wikander@gmail.com>
2018-03-28 14:33:46 +0200

    Restructured project filesystem.

--- Server/Server.java
+++ Server/Server.java
@@ -1,9 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-	
-	public static void main(String[] args) throws InterruptedException {
-		new ArduinoController(5482);
-//		new ClientController(5483);
-		Thread.sleep(1000);
-		new ArduinoEmulator(5482, "127.0.0.1");
-	}
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+
+        try {
+            System.out.println(InetAddress.getLocalHost().getHostAddress());
+            serverSocket = new ServerSocket(port);
+            while(true) {
+                clientSocket = serverSocket.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                while (true) {
+                    String line = br.readLine();
+                    if (line != null) {
+                        System.out.println(line);
+                    }
+                }
+
+            }
+        } catch (IOException e)  { }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(80);
+    }
 }
