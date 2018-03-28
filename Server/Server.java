b272820e3e6f5256cea3dde76ce12cb6ec05e3db
David Johansson <david.n.johansson@live.com>
2018-03-28 07:43:47 +0000

    Added new classes

--- Server/Server.java
+++ Server/Server.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,66 +9,69 @@ import java.net.Socket;
 import java.util.HashMap;
 
 public class Server implements Runnable {
-    private int port;
-    private ServerSocket serverSocket;
-    private Socket socket;
+	private int port;
+	private ServerSocket serverSocket;
 
-    // TODO: Ska vi lagra arduinoklienter tillsammans med användarnamn i samma hashmap?
-    private HashMap<String, ClientHandler> arduinoClients;
-    private HashMap<String, ClientHandler> desktopClients;
+	// TODO: Ska vi lagra arduinoklienter tillsammans med användarnamn i samma hashmap?
+	private HashMap<String, ClientHandler> arduinoClients;
+	private HashMap<String, ClientHandler> desktopClients;
+
+	public Server(int port) {
+		this.port = port;
+		try {
+			serverSocket = new ServerSocket(port);
+			System.out.println("Server initiated. Listening on:" + port + " IP address: "
+					+ InetAddress.getLocalHost().getHostAddress());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		new Thread(this).start();
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				Socket socket = serverSocket.accept();
+				ClientHandler clientHandler = new ClientHandler(socket);
+				clientHandler.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+
+	public class ClientHandler extends Thread {
+		private Socket socket;
+		private final String requestArduino = "Request: Arduino";
 
 
-    public Server(int port) {
-        this.port = port;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Server initiated. Listening on:" + port + " IP address: "
-                    + InetAddress.getLocalHost().getHostAddress());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        new Thread(this).start();
-    }
+		public ClientHandler(Socket socket) {
+			this.socket = socket;
+		}
 
-    public void run() {
-        while (true) {
-            try {
-                socket = serverSocket.accept();
-                ClientHandler clientHandler = new ClientHandler(socket);
-                clientHandler.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
-    public class ClientHandler extends Thread {
-        private Socket socket;
+		public void run() {
+			try (DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+					DataInputStream ois = new DataInputStream(socket.getInputStream())) {
 
-        public ClientHandler(Socket socket) {
-            this.socket = socket;
-        }
+				if (ois.readUTF().equals(requestArduino)) {
+					//TODO: kolla om arduino finns i databasen
+					
+					//
+				}
 
-        public void run() {
-            try (ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream())) {
+				while (!socket.isClosed()) {
+				}
 
-                if (ois.read() == 1) {
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-                    // TODO: Username, client?
-                    arduinoClients.put();
-                }
+	public static void main(String[] args) throws IOException {
 
-                while (!socket.isClosed()) {
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-
-    }
+	}
 }
