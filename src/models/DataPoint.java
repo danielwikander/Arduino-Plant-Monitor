0b00fa2cf14187ee1c0b8d7c76df6594274229b0
Eric <ericgrevillius@gmail.com>
2018-04-19 16:10:23 +0200

    Added Javadoc to DataPoint.java and DataRequest.java

--- src/models/DataPoint.java
+++ src/models/DataPoint.java
@@ -6,59 +6,129 @@ import java.io.Serializable;
  * Represents a collection of plant data at a specific point in time.
  */
 public class DataPoint implements Serializable {
-	
+
 	private static final long serialVersionUID = 5914959873620340574L;
 	private String timeStamp;
-    private int soilMoistureLevel;
-    private int humidity;
-    private int lightLevel;
-    private int temperature;
+	private int soilMoistureLevel;
+	private int humidity;
+	private int lightLevel;
+	private int temperature;
 
-    public DataPoint (String timeStamp, int soilMoistureLevel, int humidity, int temperature, int lightLevel) {
-        this.timeStamp = timeStamp;
-        this.soilMoistureLevel = soilMoistureLevel;
-        this.humidity = humidity;
-        this.lightLevel = lightLevel;
-        this.temperature = temperature;
-    }
+	/**
+	 * Constructor creates a DataPoint containing a time-stamp and data for the
+	 * plants humidity level, soil moisture level temperature and light-level.
+	 * 
+	 * @param timeStamp
+	 *            Time and date of the DataPoint.
+	 * @param soilMoistureLevel
+	 *            Data representing the soil moisture level.
+	 * @param humidity
+	 *            Data representing the humidity level.
+	 * @param temperature
+	 *            Data representing the temperature.
+	 * @param lightLevel
+	 *            Data representing the light level.
+	 */
+	public DataPoint(String timeStamp, int soilMoistureLevel, int humidity, int temperature, int lightLevel) {
+		this.timeStamp = timeStamp;
+		this.soilMoistureLevel = soilMoistureLevel;
+		this.humidity = humidity;
+		this.lightLevel = lightLevel;
+		this.temperature = temperature;
+	}
 
-    public String getTimeStamp() {
-        return timeStamp;
-    }
+	/**
+	 * Returns the string of the time stamp.
+	 * 
+	 * @return String of the time and date.
+	 */
+	public String getTimeStamp() {
+		return timeStamp;
+	}
 
-    public void setTimeStamp(String timeStamp) {
-        this.timeStamp = timeStamp;
-    }
+	/**
+	 * Sets the time and date of this DataPoint.
+	 * 
+	 * @param timeStamp
+	 *            String of the time and date to be set.
+	 */
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 
-    public int getSoilMoistureLevel() {
-        return soilMoistureLevel;
-    }
+	/**
+	 * Returns the value of the soil moisture level.
+	 * 
+	 * @return Integer value of the soil moisture.
+	 */
+	public int getSoilMoistureLevel() {
+		return soilMoistureLevel;
+	}
 
-    public void setSoilMoistureLevel(int soilMoistureLevel) {
-        this.soilMoistureLevel = soilMoistureLevel;
-    }
+	/**
+	 * Sets the soil moisture level.
+	 * 
+	 * @param soilMoistureLevel
+	 *            Integer value of the soil moisture to be set.
+	 */
+	public void setSoilMoistureLevel(int soilMoistureLevel) {
+		this.soilMoistureLevel = soilMoistureLevel;
+	}
 
-    public int getHumidity() {
-        return humidity;
-    }
+	/**
+	 * Returns the value of the humidity level.
+	 * 
+	 * @return Integer value of the humidity.
+	 */
+	public int getHumidity() {
+		return humidity;
+	}
 
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
+	/**
+	 * Sets the humidity level.
+	 * 
+	 * @param humidity
+	 *            Integer value of the humidity level to be set.
+	 */
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
 
-    public int getLightLevel() {
-        return lightLevel;
-    }
+	/**
+	 * Returns the value of the light level.
+	 * 
+	 * @return Integer value of the light level.
+	 */
+	public int getLightLevel() {
+		return lightLevel;
+	}
 
-    public void setLightLevel(int lightLevel) {
-        this.lightLevel = lightLevel;
-    }
+	/**
+	 * Sets the light level.
+	 * 
+	 * @param lightLevel
+	 *            Integer value of the light level to be set.
+	 */
+	public void setLightLevel(int lightLevel) {
+		this.lightLevel = lightLevel;
+	}
 
-    public int getTemperature() {
-        return temperature;
-    }
+	/**
+	 * Returns the value of the temperature
+	 * 
+	 * @return Integer value of the temperature.
+	 */
+	public int getTemperature() {
+		return temperature;
+	}
 
-    public void setTemperature(int temperature) {
-        this.temperature = temperature;
-    }
+	/**
+	 * Sets the temperature.
+	 * 
+	 * @param temperature
+	 *            Integer value of the temperature.
+	 */
+	public void setTemperature(int temperature) {
+		this.temperature = temperature;
+	}
 }
